607de9dc1ec65713c736a2001cf98fae25f1a0e3
Neeraj Patil <neeraj@Neerajs-MacBook-Pro.local>
2024-10-12 14:56:39 +0530

    added deeplink configuration

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Import the required modules
 const express = require('express');
 const useragent = require('express-useragent');
+const path = require('path');
 
 // Create an Express application
 const app = express();
@@ -16,7 +17,7 @@ app.get('/', (req, res) => {
   if (source.isAndroid) {
     return res.redirect('https://play.google.com/store/apps/details?id=dev.probity.reshimgathi&pcampaignid=web_share');
   }
-  
+
   // Check if the user is on an iOS device
   if (source.isiPhone || source.isiPad || source.isiPod) {
     return res.redirect('https://apps.apple.com/in/app/race-master-3d-car-racing/id1579072162');
@@ -24,6 +25,12 @@ app.get('/', (req, res) => {
 
   // Default message for non-Android, non-iOS devices
   res.send('Welcome to our website! Please visit the store links on your mobile device.');
+});
+
+// Route to serve the assetlinks.json file
+app.get('/.well-known/assetlinks.json', (req, res) => {
+  const filePath = path.join(__dirname, 'assetlinks.json');
+  res.sendFile(filePath);
 });
 
 // Define the port number
