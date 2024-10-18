67f4e3f02cf15db9ff60a7bb1c65940285455aa4
Neeraj Patil <neeraj@Neerajs-MacBook-Pro.local>
2024-10-18 14:05:47 +0530

    ADDED SEND OTP AND VERIFY OTP API

--- app.js
+++ app.js
@@ -1,33 +1,30 @@
-// Import the required modules
+
 const express = require('express');
 const useragent = require('express-useragent');
 const path = require('path');
 const fs = require('fs');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
-// Create an Express application
+
 const app = express();
-
-// Use the useragent middleware to parse user-agent headers
+app.use(bodyParser.json());
 app.use(useragent.express());
+const apiKey = process.env.API_KEY;
+const otpTemplateName = "otp verification";
 
-// Route for the root URL "/"
+
 app.get('/', (req, res) => {
   const source = req.useragent;
-
-  // Check if the user is on an Android device
   if (source.isAndroid) {
     return res.redirect('https://play.google.com/store/apps/details?id=dev.probity.reshimgathi&pcampaignid=web_share');
   }
-
-  // Check if the user is on an iOS device
   if (source.isiPhone || source.isiPad || source.isiPod) {
     return res.redirect('https://apps.apple.com/in/app/race-master-3d-car-racing/id1579072162');
   }
-
-  // Default message for non-Android, non-iOS devices
   res.send('Welcome to our website! Please visit the store links on your mobile device.');
 });
-
 
 
 
@@ -35,24 +32,20 @@ app.get('/home/profile/:id', (req, res) => {
     const source = req.useragent;
     const profileId = req.params.id;
     
-    
     console.log(`User visited profile with ID: ${profileId}`);
-    
-    // Check if the user is on an Android device
     if (source.isAndroid) {
         return res.redirect('https://play.google.com/store/apps/details?id=dev.probity.reshimgathi&pcampaignid=web_share');
     }
     
-    // Check if the user is on an iOS device
     if (source.isiPhone || source.isiPad || source.isiPod) {
         return res.redirect('https://apps.apple.com/in/app/race-master-3d-car-racing/id1579072162');
     }
-    
-    // Default message for non-Android, non-iOS devices
+
     res.send(`Welcome to profile ${profileId}! Please visit the store links on your mobile device.`);
 });
 
-// Route to serve the assetlinks.json file
+
+
 app.get('/.well-known/assetlinks.json', (req, res) => {
   const filePath = path.join(__dirname, 'assetlinks.json');
   res.sendFile(filePath);
@@ -64,11 +57,9 @@ app.get('/apple-app-site-association', (req, res) => {
     console.log("Requested Apple Certificate");
 
     const filePath = path.join(__dirname, 'apple-app-site-association');
-  
-    // Set the Content-Type header to application/json
+
     res.setHeader('Content-Type', 'application/json');
   
-    // Stream the file to the client
     const stream = fs.createReadStream(filePath);
     stream.pipe(res);
   });
@@ -80,16 +71,70 @@ app.get('/.well-known/apple-app-site-association', (req, res) => {
 
     const filePath = path.join(__dirname, 'apple-app-site-association');
   
-    // Set the Content-Type header to application/json
     res.setHeader('Content-Type', 'application/json');
   
-    // Stream the file to the client
     const stream = fs.createReadStream(filePath);
     stream.pipe(res);
   });
+
+
+
+// Send OTP
+app.post('/api/v1/auth/send-otp', async (req, res) => {
+  const { phoneNumber } = req.body;
+  
+  if (!phoneNumber) {
+      return res.status(400).json({ message: 'Phone number is required' });
+  }
+
+  const sendOtpUrl = `https://2factor.in/API/V1/${apiKey}/SMS/${phoneNumber}/AUTOGEN3/${otpTemplateName}`;
+
+  try {
+      const response = await axios.get(sendOtpUrl);
+      if (response.data.Status === 'Success') {
+          res.status(200).json({
+              message: 'OTP sent successfully',
+              details: response.data
+          });
+      } else {
+          res.status(500).json({ message: 'Failed to send OTP', error: response.data });
+      }
+  } catch (error) {
+      res.status(500).json({ message: 'Error sending OTP', error: error.message });
+  }
+});
+
+// Validate OTP
+app.post('/api/v1/auth/validate-otp', async (req, res) => {
+  const { phoneNumber, otp } = req.body;
+
+  if (!phoneNumber || !otp) {
+      return res.status(400).json({ message: 'Phone number and OTP are required' });
+  }
+
+  const validateOtpUrl = `https://2factor.in/API/V1/${apiKey}/SMS/VERIFY3/${phoneNumber}/${otp}`;
+
+  try {
+      const response = await axios.get(validateOtpUrl);
+      if (response.data.Status === 'Success') {
+          res.status(200).json({
+              message: 'OTP validated successfully',
+              details: response.data
+          });
+      } else {
+          res.status(400).json({ message: 'Invalid OTP', error: response.data });
+      }
+  } catch (error) {
+      res.status(500).json({ message: 'Error validating OTP', error: error.message });
+  }
+});
+
+
+
+
   
 
-// Define the port number
+
 const PORT = 3000;
 
 // Start the server
